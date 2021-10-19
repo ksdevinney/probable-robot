1996aff01896ca7a0711427ad88fd9661c8370d5
ksdevinney <katie@devinney.org>
2021-10-19 14:29:12 -0500

    about section

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector("#mobile-menu");
-const menuLinks = document.querySelector(".navbar-links");
+const menuLinks = document.querySelector(".navbar-link");
 
 const mobileMenu = () => {
     menu.classList.toggle("is-active");
